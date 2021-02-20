d9b9020b5aeacf0092db4700c9370549c8ca5b9c
ahmed <dev.ahmed.abbas@gmail.com>
2021-02-20 12:41:16 +0200

    feat: Build nav Menu items dynamically from sections

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,15 +17,36 @@
  * Define Global Variables
  * 
 */
-
+const menuId = 'navbar__list';
+const sections = document.querySelectorAll('main section');
+let navItems = [] ;
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+let buildNavItems = () => {
+    let documentFragment = new DocumentFragment();
 
+    for (let i = 0; i < sections.length; i++)
+    {
+        let li = document.createElement('li');
 
+        li.innerHTML = sections[i].getAttribute('data-nav') ;
+        li.dataset.section_id = sections[i].id;
+        li.classList.add('menu__link');
+
+        if( i === 0)
+        {
+            li.classList.add('active-li');
+        }
+
+        documentFragment.appendChild( li );
+    }
+
+    document.getElementById(`${menuId}`).appendChild( documentFragment );
+}
 
 /**
  * End Helper Functions
@@ -34,8 +55,8 @@
 */
 
 // build the nav
-
-
+buildNavItems();
+console.log( navItems );
 // Add class 'active' to section when near top of viewport
 
 
